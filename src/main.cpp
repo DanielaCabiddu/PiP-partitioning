e3cb3d910901b1c626a2cab1156510f7f14eb6ab
Daniela Cabiddu <daniela.cabiddu@cnr.it>
2025-08-08 11:47:11 +0200

    code and repo cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,6 @@
 
 #include "meshing/auxiliary.h"
 #include "io/read_GIS.h"
-#include "io/write_GIS.h"
-
-#include "meshing/dual_mesh.h"
 
 // #include "urban3D/utils/point_in_polygon.h"
 
@@ -35,42 +32,31 @@ namespace fs = std::filesystem;
 
 int main(int argc, char *argv[])
 {
-    std::string external_boundary_path   ;
-    std::string internal_boundaries_path ;
+    std::string polys_path   ;
     std::string las_path  = ""; // Not used in this example, but kept for consistency
 
     std::string output_las_folder;
-    std::string output_shp_file;
 
     uint boundary_epsg;
 
     try
     {
         // Define command line parser and arguments
-        TCLAP::CmdLine cmd("Urban-PBF", ' ', "version 0.5");
+        TCLAP::CmdLine cmd("PiP-Partitioning", ' ', "version 0.5");
 
         // Define main functionalities options
-        TCLAP::ValueArg<std::string> external_arg("e", "external", "External Boundary", true, "name_ground", "string", cmd);
-        TCLAP::ValueArg<std::string> internal_arg("i", "internal", "Internal Boundaries file", true, "name_pav", "string", cmd);
+        TCLAP::ValueArg<std::string> polys_arg("p", "polys", "Polygons", true, "name_ground", "string", cmd);
 
-        TCLAP::ValueArg<std::string> pc_arg("l", "las", "Point Cloud (LAS)", false, "name_pav", "string", cmd);
-        TCLAP::ValueArg<std::string> o_pc_arg("L", "output-las-folder", "OutputLAS folder", false, "name_pav", "string", cmd);
-
-        TCLAP::ValueArg<std::string> o_shp_arg("O", "output-shp", "Output Shp", true, "name_out", "string", cmd);
+        TCLAP::ValueArg<std::string> pc_arg("l", "las", "Point Cloud (LAS)", true, "name_pav", "string", cmd);
+        TCLAP::ValueArg<std::string> o_pc_arg("L", "output-las-folder", "OutputLAS folder", true, "name_pav", "string", cmd);
 
         // Parse the argv array
         cmd.parse(argc, argv);
 
-        external_boundary_path = external_arg.getValue();
-        internal_boundaries_path = internal_arg.getValue();
+        polys_path = polys_arg.getValue();
 
-        if (pc_arg.isSet())
-            las_path = pc_arg.getValue();
-
-        if (o_pc_arg.isSet())
-            output_las_folder = o_pc_arg.getValue();
-
-        output_shp_file = o_shp_arg.getValue();
+        las_path = pc_arg.getValue();
+        output_las_folder = o_pc_arg.getValue();
 
     }
     catch (TCLAP::ArgException &e) // catch exceptions
@@ -84,328 +70,147 @@ int main(int argc, char *argv[])
     //     assert(false);
     // }
 
-    cinolib::Polygonmesh<> external_boundary;
+    cinolib::Polygonmesh<> polys_mesh;
 
-    if (fs::path(external_boundary_path).extension().compare(".off") == 0)
-        external_boundary.load(external_boundary_path.c_str());
-    else
-        if (fs::path(external_boundary_path).extension().compare(".shp") == 0)
+    GISData gis_data;
+    Urban3D::IO::read_GIS(polys_path, gis_data);
+    boundary_epsg = gis_data.get_epsg();
+
+    polys_mesh = gis_data.convert_to_polygon_mesh();
+
+
+    // Check if the LAS file exists
+    std::ifstream ifs;
+    ifs.open(las_path.c_str(), std::ios::in | std::ios::binary);
+
+    if (ifs.is_open())
+    {
+        std::cout << "Processing LAS file: " << las_path << std::endl;
+        liblas::Reader reader(ifs);
+        liblas::Header const& header = reader.GetHeader();
+
+        int nPoints = header.GetPointRecordsCount();
+
+        std::cout << "Number of points in the LAS file: " << nPoints << std::endl;
+
+        // Store the points and the point-to-region mapping
+        std::vector<liblas::Point> Points;
+        std::vector<uint> point2region (nPoints, UINT_MAX);
+
+        std::vector<std::vector<uint>> region2point (polys_mesh.num_polys(), std::vector<uint>());
+        Points.reserve(nPoints);
+
+        while (reader.ReadNextPoint())
         {
-            GISData gis_data;
-            Urban3D::IO::read_GIS(external_boundary_path, gis_data);
-            boundary_epsg = gis_data.get_epsg();
-
-            external_boundary = gis_data.convert_to_polygon_mesh();
+            liblas::Point p = reader.GetPoint();  // safer than using a const ref
+            Points.push_back(p);
         }
-        else
+
+        std::atomic<int> lastPercentagePrinted{0};
+
+
+        #pragma omp parallel for ordered schedule(static,1)
+        for (int j = 0; j < nPoints; j++)
         {
-            std::cerr << "Unsupported input format : " << external_boundary_path << std::endl;
-            exit(1);
-        }
+            // if (!point_in_polygon(external_boundary, cinolib::vec3d(Points.at(j).GetX(), Points.at(j).GetY(),Points.at(j).GetZ()), 0))
+            //     continue;
+            // reader.ReadNextPoint();
+            // const liblas::Point p = reader.GetPoint();
+            // Points.push_back(p);
 
-    cinolib::vec3d c = external_boundary.centroid();
-    std::map<cinolib::vec3d, double> z_coordinates;
-    std::vector<uint> buildings_holes;
-    std::vector<std::string> outside_buildings;
+            // std::cout << "Processing point " << j << " with coordinates: "
+            //           << Points.at(j).GetX() << ", "
+            //           << Points.at(j).GetY() << ", "
+            //           << Points.at(j).GetZ() << std::endl;
 
-    // translate the ground to the origin and project to the plane z=0
-    external_boundary.translate(-c);
-    for (cinolib::vec3d &v : external_boundary.vector_verts()) {
-        double tmp = v.z();
-        v.z() = 0.;
-        z_coordinates[v] = tmp;
-    }
-    external_boundary.update_bbox();
 
-    cinolib::Polygonmesh<> buildings;
 
-    if (fs::path(internal_boundaries_path).extension().compare(".off") == 0)
-        buildings.load(internal_boundaries_path.c_str());
-    else
-    if (fs::path(internal_boundaries_path).extension().compare(".shp") == 0)
-    {
-        GISData gis_data;
-        Urban3D::IO::read_GIS(internal_boundaries_path, gis_data);
-
-        buildings = gis_data.convert_to_polygon_mesh();
-    }
-    else
-    {
-        std::cerr << "Unsupported input format : " << internal_boundaries_path << std::endl;
-        exit(1);
-    }
-
-    for (uint p=0; p < buildings.num_polys(); p++)
-    {
-        cinolib::Polygonmesh<> building;
-
-        std::vector<uint> poly;
-
-        for (const int v : buildings.adj_p2v(p))
-        {
-            int id = building.vert_add(buildings.vert(v));
-            poly.push_back(id);
-        }
-
-        building.poly_add(poly);
-
-        // translate the building to the origin and project to the plane z=0
-        building.translate(-c);
-        for (cinolib::vec3d &v : building.vector_verts()) {
-            double tmp = v.z();
-            v.z() = 0.;
-            z_coordinates[v] = tmp;
-        }
-
-        // // check that the building is contained within the ground polygon
-        // bool check = true;
-        // for (cinolib::vec3d &v : building.poly_verts(0)) {
-        //     if (!point_in_polygon(external_boundary, v, 0)) {
-        //         check = false;
-        //         outside_buildings.push_back(fs::path(building_path).filename().string());
-        //         break;
-        //     }
-        // }
-        // if (!check) continue;
-
-        // merge the building with the ground
-        cinolib::Polygonmesh<> tmp = external_boundary;
-        merge_meshes_at_coincident_vertices(tmp, building, external_boundary);
-
-        // add the building id to the buildings_holes list
-        std::vector<uint> vlist;
-        for (cinolib::vec3d &v : building.poly_verts(0)) {
-            uint vid = external_boundary.pick_vert(v);
-            vlist.push_back(vid);
-        }
-        int pid = external_boundary.poly_id(vlist);
-        assert(pid >= 0);
-        buildings_holes.push_back(pid);
-    }
-
-    // triangulate the ground with the buildings holes
-    cinolib::Trimesh<> ground_tri = triangulate_with_holes(external_boundary, buildings_holes);
-
-    // translate and project back to the original position
-    for (cinolib::vec3d &v : ground_tri.vector_verts()) {
-        auto it = z_coordinates.find(v);
-        v.z() = it->second;
-    }
-    ground_tri.translate(c);
-    ground_tri.update_bbox();
-    // ground_tri.save(output_path.c_str());
-
-    // print the buildings outside the ground polygon
-    if (outside_buildings.size() > 0) {
-        std::cout << "WARNING: buildings outside the ground polygon:" << std::endl;
-        for (const std::string &s : outside_buildings) {
-            std::cout << s << std::endl;
-        }
-    }
-
-    cinolib::DrawablePolygonmesh<> dual_m;
-    cinolib::DrawableTrimesh<> dual_tri;
-    URBAN3D::dual_mesh(ground_tri, dual_m/*, dual_tri*/);
-
-    std::cout << "Dual_m size (#polys) :: " << dual_m.num_polys() << std::endl;
-
-    dual_m.show_wireframe(true);
-    dual_m.show_marked_edge_color(cinolib::Color::BLUE());
-    dual_m.show_vert_color();
-    dual_m.updateGL_marked();
-    dual_m.updateGL();
-
-    dual_tri.show_poly_color();
-    dual_tri.updateGL();
-
-    GISData shapefile (dual_m, boundary_epsg);
-    URBAN3D::write_GIS(output_shp_file, shapefile);
-
-#ifdef USE_CINOLIB_GUI
-
-    cinolib::DrawableTrimesh<> ground_tri_DM(ground_tri.vector_verts(), ground_tri.vector_polys());
-    // ground_tri_DM.translate(-c);
-    ground_tri_DM.edge_mark_boundaries();
-    ground_tri_DM.updateGL();
-    ground_tri_DM.show_marked_edge_width(7.5);
-
-    cinolib::GLcanvas gui(1000, 1000);
-    gui.push(&ground_tri_DM);
-    gui.push(&dual_m);
-    gui.push(&dual_tri);
-
-    for (uint vid=0; vid < dual_m.num_verts(); vid++)
-        gui.push_marker(cinolib::vec3d(dual_m.vert(vid)));
-
-    cinolib::DrawableSegmentSoup ss;
-
-    for (uint eid=0; eid < dual_m.num_edges(); eid++)
-        ss.push_seg(dual_m.edge_vert(eid,0), dual_m.edge_vert(eid,1));
-
-    ss.default_color = cinolib::Color::BLUE();
-    ss.use_gl_lines = true;
-    ss.thickness = 5.;
-
-    gui.push(&ss);
-
-    cinolib::SurfaceMeshControls<cinolib::DrawableTrimesh<>> menu(&ground_tri_DM, &gui, "Ground");
-    gui.push(&menu);
-
-    if (las_path.length() == 0)
-    {
-        cinolib::SurfaceMeshControls<cinolib::DrawablePolygonmesh<>> menu2(&dual_m, &gui, "Dual Mesh");
-        return gui.launch();
-    }
-
-    gui.launch();
-
-#endif
-
-/////////////////////////////////////////
-///
-///
-    if (las_path.length() > 0)
-    {
-        // Check if the LAS file exists
-        std::ifstream ifs;
-        ifs.open(las_path.c_str(), std::ios::in | std::ios::binary);
-
-        if (ifs.is_open())
-        {
-            std::cout << "Processing LAS file: " << las_path << std::endl;
-            liblas::Reader reader(ifs);
-            liblas::Header const& header = reader.GetHeader();
-
-            int nPoints = header.GetPointRecordsCount();
-
-            std::cout << "Number of points in the LAS file: " << nPoints << std::endl;
-
-            // Store the points and the point-to-region mapping
-            std::vector<liblas::Point> Points;
-            std::vector<uint> point2region (nPoints, UINT_MAX);
-
-            std::vector<std::vector<uint>> region2point (shapefile.get_polygons().size(), std::vector<uint>());
-            Points.reserve(nPoints);
-
-            while (reader.ReadNextPoint())
+            for (uint pid=0; pid < polys_mesh.num_polys(); pid++)
             {
-                liblas::Point p = reader.GetPoint();  // safer than using a const ref
-                Points.push_back(p);
+                if (point_in_polygon(polys_mesh, cinolib::vec3d(Points.at(j).GetX(), Points.at(j).GetY(),Points.at(j).GetZ()), pid))
+                {
+                    // region2point.at(pid).push_back(j);
+                    point2region.at(j) = pid;
+                    // std::cout << "Point " << j << " belongs to region " << pid << std::endl;
+                    break;
+                }
             }
 
-            cinolib::Polygonmesh<> shp_mesh = shapefile.convert_to_polygon_mesh();
+            int currentPercentage = static_cast<int>((100.0 * j) / nPoints);
 
-            std::atomic<int> lastPercentagePrinted{0};
-
-
-            #pragma omp parallel for ordered schedule(static,1)
-            for (int j = 0; j < nPoints; j++)
+            // Only one thread at a time checks & updates this
+            if (j==0 || currentPercentage > lastPercentagePrinted+4) // Update every 5%)
             {
-                // if (!point_in_polygon(external_boundary, cinolib::vec3d(Points.at(j).GetX(), Points.at(j).GetY(),Points.at(j).GetZ()), 0))
-                //     continue;
-                // reader.ReadNextPoint();
-                // const liblas::Point p = reader.GetPoint();
-                // Points.push_back(p);
-
-                // std::cout << "Processing point " << j << " with coordinates: "
-                //           << Points.at(j).GetX() << ", "
-                //           << Points.at(j).GetY() << ", "
-                //           << Points.at(j).GetZ() << std::endl;
-
-
-
-                for (uint pid=0; pid < shp_mesh.num_polys(); pid++)
+                // atomic compare-and-swap: only one thread will succeed
+                int expected = lastPercentagePrinted;
+                if (lastPercentagePrinted.compare_exchange_strong(expected, currentPercentage))
                 {
-                    if (point_in_polygon(shp_mesh, cinolib::vec3d(Points.at(j).GetX(), Points.at(j).GetY(),Points.at(j).GetZ()), pid))
+                    #pragma omp ordered
                     {
-                        // region2point.at(pid).push_back(j);
-                        point2region.at(j) = pid;
-                        // std::cout << "Point " << j << " belongs to region " << pid << std::endl;
-                        break;
-                    }
-                }
+                        std::stringstream ss;
+                        ss << "Processed " << j << " points / " << nPoints
+                           << " total points (" << currentPercentage << "%)";
 
-                int currentPercentage = static_cast<int>((100.0 * j) / nPoints);
+                        if (currentPercentage == 100)
+                            ss << " - done!";
+                        else
+                            ss << "...";
 
-                // Only one thread at a time checks & updates this
-                if (j==0 || currentPercentage > lastPercentagePrinted+4) // Update every 5%)
-                {
-                    // atomic compare-and-swap: only one thread will succeed
-                    int expected = lastPercentagePrinted;
-                    if (lastPercentagePrinted.compare_exchange_strong(expected, currentPercentage))
-                    {
-                        #pragma omp ordered
-                        {
-                            std::stringstream ss;
-                            ss << "Processed " << j << " points / " << nPoints
-                               << " total points (" << currentPercentage << "%)";
-
-                            if (currentPercentage == 100)
-                                ss << " - done!";
-                            else
-                                ss << "...";
-
-                            std::cout << ss.str() << std::endl;
-                        }
+                        std::cout << ss.str() << std::endl;
                     }
                 }
             }
-
-            for (int j = 0; j < nPoints; j++)
-            {
-                if (point2region.at(j) < UINT_MAX)
-                    region2point.at(point2region.at(j)).push_back(j);
-            }
-
-            ///
-            for (uint pid=0; pid < shapefile.get_polygons().size(); pid++)
-            {
-                if (region2point.at(pid).size() == 0)
-                {
-                    std::cout << "Region " << pid << " has no points." << std::endl;
-                    continue;
-                }
-
-                std::ofstream outFile;
-
-                std::string outFolder = output_las_folder + "/" + std::to_string(pid);
-                std::string outName = outFolder + "/" + std::to_string(pid) + ".las";
-
-                // create output directory if it does not exist
-                if (!fs::exists(outFolder))
-                {
-                    fs::create_directories(outFolder);
-                }
-
-                std::cout << "Writing LAS file: " << outName << std::endl;
-
-                outFile.open(outName, std::ios::out | std::ios::binary);
-                if (!outFile.is_open())
-                {
-                    std::cerr << "Error opening output LAS file: " << outName << std::endl;
-                    continue;
-                    // exit(1);
-                }
-
-                liblas::Header h = header;
-                h.SetPointRecordsCount(region2point.at(pid).size());
-                liblas::Writer *writer = new liblas::Writer(outFile, h);
-
-                for (uint i=0; i < region2point.at(pid).size(); i++)
-                {
-                    writer->WritePoint(Points.at(region2point.at(pid).at(i)));
-                }
-
-                outFile.close();
-            }
-
-            ifs.close();
         }
-        else
+
+        for (int j = 0; j < nPoints; j++)
         {
-            std::cerr << "Error opening LAS file: " << las_path << std::endl;
-            exit(1);
+            if (point2region.at(j) < UINT_MAX)
+                region2point.at(point2region.at(j)).push_back(j);
         }
+
+        ///
+        for (uint pid=0; pid < polys_mesh.num_polys(); pid++)
+        {
+            if (region2point.at(pid).size() == 0)
+            {
+                std::cout << "Region " << pid << " has no points." << std::endl;
+                continue;
+            }
+
+            std::ofstream outFile;
+
+            std::string outFolder = output_las_folder + "/" + std::to_string(pid);
+            std::string outName = outFolder + "/" + std::to_string(pid) + ".las";
+
+            // create output directory if it does not exist
+            if (!fs::exists(outFolder))
+            {
+                fs::create_directories(outFolder);
+            }
+
+            std::cout << "Writing LAS file: " << outName << std::endl;
+
+            outFile.open(outName, std::ios::out | std::ios::binary);
+            if (!outFile.is_open())
+            {
+                std::cerr << "Error opening output LAS file: " << outName << std::endl;
+                continue;
+                // exit(1);
+            }
+
+            liblas::Header h = header;
+            h.SetPointRecordsCount(region2point.at(pid).size());
+            liblas::Writer *writer = new liblas::Writer(outFile, h);
+
+            for (uint i=0; i < region2point.at(pid).size(); i++)
+            {
+                writer->WritePoint(Points.at(region2point.at(pid).at(i)));
+            }
+
+            outFile.close();
+        }
+
+        ifs.close();
     }
 
 
--- src/meshing/auxiliary.h
+++ src/meshing/auxiliary.h
@@ -1,49 +1,7 @@
-/**
- *
- * Tommaso Sorgente
- * tommaso.sorgente@cnr.it
- *
-**/
-
 #ifndef CITY_AUXILIARY
 #define CITY_AUXILIARY
 
 #include <cinolib/meshes/meshes.h>
-#include <cinolib/triangle_wrap.h>
-
-double TOLL = 1e-6;
-
-inline
-cinolib::Trimesh<> triangulate_with_holes(const cinolib::Polygonmesh<> &m, const std::vector<uint> &holes)
-{
-    std::vector<cinolib::vec2d> verts_in;
-    for(uint vid=0; vid<m.num_verts(); ++vid) {
-        verts_in.push_back(cinolib::vec2d(m.vert(vid).x(), m.vert(vid).y()));
-    }
-
-    std::vector<uint>  edges_in;
-    for(uint eid=0; eid<m.num_edges(); ++eid) {
-        edges_in.push_back(m.edge_vert_id(eid,0));
-        edges_in.push_back(m.edge_vert_id(eid,1));
-    }
-
-    std::vector<cinolib::vec2d> holes_centers;
-    for(uint pid : holes) {
-        std::vector<uint> tess = m.poly_tessellation(pid);
-        cinolib::vec3d v0 = m.vert(tess.at(0));
-        cinolib::vec3d v1 = m.vert(tess.at(1));
-        cinolib::vec3d v2 = m.vert(tess.at(2));
-        cinolib::vec3d c = (v0 + v1 + v2) / 3.;
-        holes_centers.push_back(cinolib::vec2d(c.x(), c.y()));
-    }
-
-    cinolib::Trimesh<> m_tri;
-    std::string t_flags = "QY";
-    // double area = 10. * pow(m.edge_avg_length(), 2);
-    // t_flags += "qa" + std::to_string(area);
-    triangle_wrap(verts_in, edges_in, holes_centers, 0., t_flags.c_str(), m_tri);
-    return m_tri;
-}
 
 
 inline
